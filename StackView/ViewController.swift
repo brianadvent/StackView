7d15cf09f5cf507e92cc08076b9a9ee5e13c4cd5
Training <training@daniels-imac-7.fritz.box>
2016-06-03 15:11:46 +0200

    StackView Tutorial

--- StackView/ViewController.swift
+++ StackView/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showDescription(sender: AnyObject) {
+        
+        let button = sender as! UIButton
+        
+        if button.titleLabel?.text == "Show description" {
+            button.setTitle("Hide description", forState: .Normal)
+            
+            UIView.animateWithDuration(0.3, animations: { 
+                self.descriptionLabel.hidden = false
+            })
+            
+        }else{
+            button.setTitle("Show description", forState: .Normal)
+            
+            UIView.animateWithDuration(0.3, animations: {
+                self.descriptionLabel.hidden = true
+            })
+            
+        }
+        
+        
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
